6b35b30f6e1d87a779ee0dfc9098cdc39e3d4482
Son <haison26-6@gmail.com>
2025-10-07 15:58:13 +0700

    2nd commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+const engine = exphbs.engine
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname,'resouces/views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
