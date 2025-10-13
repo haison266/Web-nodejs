3c5054b8dab35e3d155843cf1ff0958153b9e9f6
Son <haison26-6@gmail.com>
2025-10-13 21:17:09 +0700

    Remove css filr

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 const engine = exphbs.engine
+
+
+app.use(express.static(path.join(__dirname,'public')))
+
 //HTTP logger
 app.use(morgan('combined'))
 
@@ -13,8 +17,7 @@ app.engine('hbs', engine({
   extname: '.hbs'
 }))
 app.set('view engine', 'hbs')
-app.set('views',path.join(__dirname,'resouces/views'))
-
+app.set('views',path.join(__dirname,'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
