a786837ffc79d1f2fba555b60d4f145cea9a8c9c
Son <haison26-6@gmail.com>
2025-10-21 16:47:25 +0700

    Update route + Controller

--- src/app.js
+++ src/app.js
@@ -5,29 +5,25 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 const engine = exphbs.engine
+const route  = require('./routes/')
 
-
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));            
 
 //Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
 }))
-app.set('view engine', 'hbs')
-app.set('views',path.join(__dirname,'resources/views'))
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//Route init
+route(app);
 
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {  
+  console.log(`Example app listening on port ${port}`);
 })
---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,16 @@
+
+class SiteController {
+
+    //Get homepage  
+    home(req, res) {
+        res.render('home');
+    }
+
+    // Get /search
+    search(req, res) {  
+        res.render('search');  
+    }   
+    
+}
+
+module.exports = new SiteController();
