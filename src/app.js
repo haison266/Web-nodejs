f8b491f67cfb6736f6da72b739547ab4c7f05b1b
Son <haison26-6@gmail.com>
2025-10-24 22:32:41 +0700

    Testing Husky

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
-const engine = exphbs.engine
-const route  = require('./routes/')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const engine = exphbs.engine;
+const route = require('./routes/');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-                  app.use(express.json());  
+
+app.use(express.json());
 
 //HTTP logger
-app.use(morgan('combined'));            
+app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-
-
-
-
-
-  
-}))
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    })
+);
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route init
 route(app);
 
-app.listen(port, () => {  
-  console.log(`Example app listening on port ${port}`);
-})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
