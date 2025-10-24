d2ed8a79f96f450bbc65bd118306eda86a7a52c9
Son <haison26-6@gmail.com>
2025-10-24 21:41:11 +0700

    testing husky

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const route  = require('./routes/')
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());  
+                  app.use(express.json());  
 
 //HTTP logger
 app.use(morgan('combined'));            
@@ -17,6 +17,12 @@ app.use(morgan('combined'));
 //Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
+
+
+
+
+
+  
 }))
 app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'resources/views'));
--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,17 +1,13 @@
-
 class NewsController {
-
     //Get /news
     index(req, res) {
         res.render('news');
     }
 
     // Get /news/:slug
-    show(req, res) {  
-        res.send('NEWS DETAIL!!!');  
-    }   
-
-
+    show(req, res) {
+        res.send('NEWS DETAIL!!!');
+    }
 }
 
 module.exports = new NewsController();
